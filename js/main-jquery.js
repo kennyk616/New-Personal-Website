ba0c3df4bf0888c0ca6a30c398083469a63ee0e0
kkao <kkao@palantir.com>
2012-07-10 04:05:42 -0700

    add other in projects section

--- js/main-jquery.js
+++ js/main-jquery.js
@@ -1,66 +1,37 @@
-window.onload = function() {
-	// Code if document.getElementByClassName() doesn't exist
-	if (document.getElementsByClassName == undefined) {
-		document.getElementsByClassName = function(className) {
-			var hasClassName = new RegExp("(?:^|\\s)" + className + "(?:$|\\s)");
-			var allElements = document.getElementsByTagName("*");
-			var results = [];
-
-			var element;
-			for (var i = 0; ( element = allElements[i]) != null; i++) {
-				var elementClass = element.className;
-				if (elementClass && elementClass.indexOf(className) != -1 && hasClassName.test(elementClass))
-					results.push(element);
-			}
-
-			return results;
-		}
-	}
-
-	var list_of_links = document.getElementsByClassName('targetblank');
-	for (var i in list_of_links) {
-		list_of_links[i].target = '_blank';
-	}
-}
-
 $(document).ready(function() {
 	jQuery('.jcarousel-skin-tango').jcarousel({
 		scroll : 2
 	});
-	switchtab("#tab6", "#tab5");
-	jQuery('#tab-projects a, #tab-about a').click(function(e) {
-		e.preventDefault();
-		jQuery(this).tab('show');
-	})
-	init();
-	//turn off scrollbar and scrollpath
-	//$("html").css("overflow-y", "hidden");
-	
+	initScrollPath();
+    initializeProjectTabs();
+    initializeLinks();
+		
 });
 
-function switchtab(hidetab, showtab) {
-	$(hidetab).css("opacity", "0");
-	$(showtab).css("opacity", "1");
-	$(showtab + " .youtube").css("display", "inline");
-	$(hidetab + " .youtube").css("display", "none");
-	$(hidetab).css("z-index", "-1");
-	$(showtab).css("z-index", "1");
+function initializeProjectTabs(){
+    $("#tab6").css("display", "none");
+    $("#tab7").css("display", "none");
 }
 
-function init() {
+function switchtab(showtab_id) {
+    $(".projects .active.tab-pane").css("display", "none");
+    $(showtab_id).css("display", "block");
+	}
+
+function initScrollPath() {
 	/* ========== DRAWING THE PATH AND INITIATING THE PLUGIN ============= */
 
 	$.fn.scrollPath("getPath")
-		// Move to 'start' element
-		.moveTo(400, 50, {name: "start"})
-		// Line to 'description' element
+		// Move to 'home' element
+		.moveTo(400, 50, {name: "home"})
+		// Line to 'projects' element
 		.lineTo(600, 850, {name: "projects"})
-		// Arc down and line to 'syntax'
+		// Arc down and line to 'aboutme'
 		.arc(200, 1200, 400, -Math.PI/2, Math.PI/2, true)
 		.lineTo(600, 1650, {
 			name: "aboutme"
 		})
-		// Continue line to 'scrollbar'
+		// Continue line to 'contact'
 		.lineTo(2250, 1850, {
 			name: "contact"
 		})
@@ -69,17 +40,16 @@ function init() {
 	// We're done with the path, let's initate the plugin on our wrapper element
 	$(".wrapper").scrollPath({drawPath: true, wrapAround: true});
 
-	// Add scrollTo on click on the navigation anchors and homepage links
+	// Add scrollTo on click on the navigation anchors, homepage links, and paragraph links
 	replacePath($("nav"));
 	replacePath($(".home"));
-	replacePath($("p"));
-	
+	replacePath($("p"));	
 	/* ===================================================================== */
 
 }
 
-function replacePath(div) {
-	div.find("a").each(function()	{
+function replacePath(elem) {
+	elem.find("a").each(function()	{
 	var link = $(this).attr("href");
 	if(link[0]=="#"){
 		var target = $(this).attr("href").replace("#", "");
@@ -92,4 +62,10 @@ function replacePath(div) {
 		});
 	}
 	});
+}
+function initializeLinks() {
+	var list_of_links = $('.targetblank');
+	for (var i in list_of_links) {
+		list_of_links[i].target = '_blank';
+	}
 }
