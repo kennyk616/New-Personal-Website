66023385bbe9537c0c676fdbfeca2b1e0ad62968
Kenny Kao <kennyk616@gmail.com>
2012-07-12 02:43:49 -0700

    disable key scrolling in firefox

--- js/main-jquery.js
+++ js/main-jquery.js
@@ -8,6 +8,22 @@ $(document).ready(function() {
 		
 });
 
+//this is only for firefox
+$("html").keydown(function(event) {
+    switch(event.keyCode) {
+        case 32://space
+        case 33://pgup
+        case 34://pgdn
+        case 35://end
+        case 36://home
+        case 37://left
+        case 38://up
+        case 39://right
+        case 40://down
+            return false;
+    }
+});
+
 function initializeProjectTabs(){
     $("#tab6").css("visibility", "hidden");
     $("#tab7").css("visibility", "hidden");
